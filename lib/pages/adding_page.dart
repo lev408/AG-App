1642d8435ebafc6681f7cc41f3a3323f33095a30
ZEF-AWT <pangzwr@gmail.com>
2024-11-23 14:29:08 +0100

    .

--- lib/pages/adding_page.dart
+++ lib/pages/adding_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class AddingPage extends StatelessWidget {
   const AddingPage({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
